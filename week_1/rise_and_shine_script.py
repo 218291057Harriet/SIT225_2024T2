9d94748c704633127c7a62e418d94b33522c1e5b
218291057Harriet <s218291057@deakin.edu.au>
2024-07-22 21:21:57 +1000

    file relocation 1

--- week_1/rise_and_shine_script.py
+++ week_1/rise_and_shine_script.py
@@ -21,19 +21,13 @@ while True:  # infinite loop, keep running
     d = s.write(bytes(str(data_send), 'utf-8'))
     print(f"{datetime.now()}: Send >>> {data_send} ({d} bytes)")
 
-    s.flush()
-
     # Read from serial port. 
     d = s.readline().decode("utf-8").strip()
     print(f"{datetime.now()}: Recv <<< {d}")
 
     # Sleep for time received, print date and time.
-    try:
-        sleepy_time = int(float(d))
-        print(f"{datetime.now()}: Sleeping for {sleepy_time} seconds")
-        time.sleep(sleepy_time)
-        print(f"{datetime.now()}: Awake now!")
-    except ValueError:
-        # Retry if the integer d is invalid
-        print(f"{datetime.now()}: Error: Invalid integer '{d}' received from Arduino. Naughty Arduino")
-
+    sleepy_time = int(float(d))
+    print(f"{datetime.now()}: Sleeping for {sleepy_time} seconds")
+    time.sleep(sleepy_time)
+    print(f"{datetime.now()}: Awake now!")
+  
